02aaa35fc7079660660b9a993c6b97dbbb5d763e
Denis Peplin <denis.peplin@gmail.com>
2014-12-12 09:28:01 +0300

    Copy helper to get datatable display length from Ruby.

--- app/helpers/datatable_state/application_helper.rb
+++ app/helpers/datatable_state/application_helper.rb
@@ -1,4 +1,9 @@
 module DatatableState
   module ApplicationHelper
+    def saved_display_length
+      saved_state = Store.find_by_user_id_and_path(current_user.id,
+        request.path.split('/').last)
+      "data-display-length=#{JSON.parse(saved_state.state)['length']}" if saved_state
+    end
   end
 end
